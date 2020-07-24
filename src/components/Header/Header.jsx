03f51394c1183b8f0b36fb2d2b1ae4f417c0dac9
aarnadlr <aaronadler@gmail.com>
2020-07-24 09:09:20 -0400

    Add responsive Header layout.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,35 +7,63 @@ import PropTypes from 'prop-types';
 export default function Header({ logo, fbLink, twLink, igLink }) {
   return (
     <section className="Header">
+
+
       <div className="red-stroke" />
+
+
       <div className="Header__black-container">
+
 
         <img src={logo && logo} alt="" />
 
-        <h2>HOME OF THE</h2>
-        <h1>NEW YORK YANKEES</h1>
 
-        <div className="Header__social-links-group">
 
-          <a className="fbLink" href={fbLink} rel="noopener noreferrer" target="_blank">
-            <Facebook />
-          </a>
-          <a className="twLink" href={twLink} rel="noopener noreferrer" target="_blank">
-            <Twitter />
-          </a>
-          <a className="igLink" href={igLink} rel="noopener noreferrer" target="_blank">
-            <Instagram />
-          </a>
+        <div className="Header__flex-wrapper">
+          <h2>HOME OF THE</h2>
+          <h1>NEW YORK YANKEES</h1>
 
+          <div className="Header__social-links-group">
+            <a
+              className="fbLink"
+              href={fbLink}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Facebook />
+            </a>
+            <a
+              className="twLink"
+              href={twLink}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Twitter />
+            </a>
+            <a
+              className="igLink"
+              href={igLink}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Instagram />
+            </a>
+          </div>
         </div>
+
+
+
+        
       </div>
+
+
     </section>
   );
-};
+}
 
 Header.propTypes = {
   logo: PropTypes.string,
   fbLink: PropTypes.string,
   twLink: PropTypes.string,
-  igLink: PropTypes.string
+  igLink: PropTypes.string,
 };
